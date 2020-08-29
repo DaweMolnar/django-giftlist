dd0a21fb27294a987e3aec27129de04cb059eccd
unknown <dawemolnar@gmail.com>
2020-08-29 23:19:04 +0100

    Add new models

--- gift_shop/shop/models.py
+++ gift_shop/shop/models.py
@@ -10,12 +10,33 @@ class Product(models.Model):
     def __str__(self):
         return str(self.name)
 
-class Gift(models.Model):
+class Couple(models.Model):
+    login_name = models.CharField(max_length=50)
+    login_password = models.CharField(max_length=256)
     groom_name = models.CharField(max_length=100, default="Andy Groom")
     bride_name = models.CharField(max_length=100, default="Beatrix Kiddo")
+    email = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=30)
+
+    def __str__(self):
+        return str(self.login_name)
+
+
+class Gift_list(models.Model):
+    name = models.CharField(max_length=128)
+    creation_date = models.DateTimeField(auto_now_add=True, editable=False)
+    couple = models.ForeignKey(Couple, related_name="gift_lists", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Gift_item(models.Model):
     added = models.DateTimeField(auto_now_add=True, editable=False)
     bought = models.BooleanField(default=False)
+    note = models.CharField(max_length=500)
+    gift_list = models.ForeignKey(Gift_list, related_name="gifts", on_delete=models.CASCADE)
     product = models.ForeignKey(Product, related_name="gifts", on_delete=models.CASCADE)
 
     def __str__(self):
-        return str(self.groom_name + ' & ' + self.bride_name)
+        return str(self.product.name)

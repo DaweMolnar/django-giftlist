80a8fdc449e9344f042945322f2a119c04841acd
unknown <dawemolnar@gmail.com>
2020-09-01 22:47:38 +0100

    Add authentication requirement

--- gift_shop/shop/urls.py
+++ gift_shop/shop/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.decorators import login_required
 
 app_name = 'shop'
 
 urlpatterns = [
-    path('', views.ProductListView.as_view(), name='list'),
+    path('', login_required(views.ProductListView.as_view()), name='list'),
     path('gifts/', views.GiftListView.as_view(), name='gifts'),
-    path('report/', views.ReportView.as_view(), name='report'),
+    path('report/', login_required(views.ReportView.as_view()), name='report'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('logout/', views.user_logout, name='logout'),
     path('<int:pk>/', views.ProductDetailView.as_view(), name='detail'),
 ]
